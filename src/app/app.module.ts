fb18086efb59fd472480e6af9dee10ebeb7b37dd
Denis Nikolaev <niko.den.vl@gmail.com>
2025-03-06 18:12:28 +0400

    fix: smooth scroll for the menu links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 
@@ -11,7 +10,6 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
